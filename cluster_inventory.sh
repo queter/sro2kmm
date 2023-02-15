fb01dd28c17003509e1917538e58c3ea9508230a
Queter <queter@gmail.com>
2023-02-15 08:34:07 +0000

    Added mykeys directory

--- cluster_inventory.sh
+++ cluster_inventory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ANSIBLE_USER=core
-ANSIBLE_SSH_KEY=nodes.pem
+ANSIBLE_SSH_KEY=mykeys/ebelarte-aws.pem
 
 MASTERS=$(oc get nodes --selector='node-role.kubernetes.io/master' -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
 WORKERS=$(oc get nodes --selector='!node-role.kubernetes.io/master' -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
